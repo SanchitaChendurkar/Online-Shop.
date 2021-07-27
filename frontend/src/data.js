5e99106946fd5e62a685bbb83417c93f6cf50bf8
SanchitaChendurkar <88021579+SanchitaChendurkar@users.noreply.github.com>
2021-07-27 13:15:53 +0530

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+    {   
+        _id:'1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price:  3000,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {   
+        _id:'2',
+        name: 'T-Shirt',
+        category: 'T-Shirts',
+        image: '/images/d2.jpeg',
+        price: 1000,
+        brand: 'Polo',
+        rating: 4.2,
+        numReviews: 5
+    },
+    {   
+        _id:'3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 2000,
+        brand: 'Nike',
+        rating: 4.8,
+        numReviews: 8
+    },
+    {   
+        _id:'4',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 2000,
+        brand: 'Nike',
+        rating: 4.8,
+        numReviews: 8
+    },
+]
+}
